28a80fa7f1a21a067b346f2c664fd955c2e20e51
Alex Mitzman <amitzman10@gmail.com>
2021-03-15 20:14:57 -0400

    initial commit

---
+++ src/main/java/com/mafia/meatball/MeatballApplication.java
@@ -0,0 +1,13 @@
+package com.mafia.meatball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatballApplication.class, args);
+	}
+
+}
